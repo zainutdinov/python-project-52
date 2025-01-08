45fc675c3d7bbf68c50b132772d15f13727d2fd0
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-01-08 14:18:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -o errexit
-
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 uv install
-uv run python manage.py collectstatic --noinput
-uv run python manage.py migrate
