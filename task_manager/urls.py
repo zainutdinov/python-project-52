6f70861b6d235d373c6c38366b08e11321d5be3b
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2025-01-09 15:02:52 +0300

    Add IndexView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from task_manager import views
+from task_manager.views import IndexView
 
 urlpatterns = [
-    path('', views.index),
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
